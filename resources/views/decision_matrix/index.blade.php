bd11d9b00f8afd82ce570b2ac83a6d8df73510db
taufiqyfirdaus <taufiqyfirdaus@gmail.com>
2023-12-12 12:44:52 +0700

    Fix content, decision matrix

--- resources/views/decision_matrix/index.blade.php
+++ resources/views/decision_matrix/index.blade.php
@@ -12,43 +12,55 @@
             </div>
         </div>
     </div>
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <table id="mytable" class="display nowrap table table-striped table-bordered" style="background-color:white">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        @foreach ($kriteria as $k)
-                                            <th>{{ $k->kode }}</th>
-                                        @endforeach
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($alternatif as $a)
-                                        <tr>
-                                            <td>{{ $a->kode }}</td>
-                                            @foreach ($kriteria as $k)
-                                                <td>
-                                                    @php
-                                                        $s = $skor->where('alternatif_id', $a->id)->where('kriteria_id', $k->id)->first();
-                                                    @endphp
-                                                    {{ $s ? $s->skor : '' }}
-                                                </td>
-                                            @endforeach
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+    <div class="content" style="margin-left: 15px">
+        <div class="breadcome-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="breadcome-list">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-6 col-sm-6 col-xs-6">
+                                    <div class="card-header border-0">
+                                        <div class="d-flex justify-content-between">
+                                            <h2 class="card-title" style="color: white">Matriks Keputusan</h2>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <table id="mytable" class="display nowrap table table-striped table-bordered" style="background-color:white">
+                                            <thead>
+                                                <tr>
+                                                    <th>No</th>
+                                                    @foreach ($kriteria as $k)
+                                                        <th>{{ $k->kode }}</th>
+                                                    @endforeach
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($alternatif as $a)
+                                                    <tr>
+                                                        <td>{{ $a->kode }}</td>
+                                                        @foreach ($kriteria as $k)
+                                                            <td>
+                                                                @php
+                                                                    $s = $skor->where('alternatif_id', $a->id)->where('kriteria_id', $k->id)->first();
+                                                                @endphp
+                                                                {{ $s ? $s->skor : '' }}
+                                                            </td>
+                                                        @endforeach
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
 
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,8 +18,8 @@
                                 <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
                                     <div class="menu-switcher-pro" style="margin-left: 10px">
                                         <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
-                                                <i class="icon nalika-menu-task"></i>
-                                            </button>
+                                            <i class="icon nalika-menu-task"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -28,6 +28,4 @@
                 </div>
             </div>
         </div>
-        
     </div>
-</div>
